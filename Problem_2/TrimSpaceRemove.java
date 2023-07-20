247048b3f0892366f39300dda84a211a4d563bf5
Masud_Rana <masudranaorg71@gmail.com>
2023-07-20 09:55:18 +0600

    commit

--- Problem_2/TrimSpaceRemove.java
+++ Problem_2/TrimSpaceRemove.java
@@ -4,7 +4,6 @@ public class TrimSpaceRemove {
         public static void main(String[] args) {
             String input = "Remove all spaces from this string.";
             String noSpacesString = input.replace(" ", "");
-
             System.out.println("Original String: \"" + input + "\"");
             System.out.println("String After spaces removed: \"" + noSpacesString + "\"");
         }
